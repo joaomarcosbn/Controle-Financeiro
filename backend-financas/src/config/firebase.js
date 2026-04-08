389354d89b720b7f4ad908cfc640f285743e814e
João Marcos Batista Nunes <joaomarcosn@ymail.com>
2026-04-08 16:01:55 -0300

    Corrigindo autenticação do Firebase no Render

--- backend-financas/src/config/firebase.js
+++ backend-financas/src/config/firebase.js
@@ -1,5 +1,15 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../../firebase-key.json'); // O arquivo que você baixou
+
+let serviceAccount;
+
+// Verifica se está rodando no Render (produção) ou no seu PC (desenvolvimento)
+if (process.env.FIREBASE_CREDENTIALS) {
+  // Em produção, ele lê do "cofre" do Render e transforma em objeto
+  serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+} else {
+  // No seu PC, ele continua lendo o arquivo físico normalmente
+  serviceAccount = require('../../firebase-key.json');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
